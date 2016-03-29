f19fff286afce956ac6b33d96bf0f48d33fb5bfb
026rus <026rus@gmail.com>
2016-03-29 04:17:19 +0200

    working on the AKI in progress

--- code_snippets/test.c
+++ code_snippets/test.c
@@ -12,6 +12,7 @@ typedef struct
 /******************************************************************************/
 bool isInside(point a, point b, point c, point d);
 void collectPoints(point **a, int *, unsigned rank);
+void aki(point **a, int *size_a, point **ex, int *size_ex );
 /******************************************************************************/
 int main(int argc, char** argv)
 { 
@@ -45,20 +46,88 @@ int main(int argc, char** argv)
 
 
 	double start = MPI_Wtime();
-    // MPI_Bcast(&p, 5, point_type, 0, comm);
+    MPI_Bcast(&numpoints, 1, MPI_INT, 0, comm);
+	
+	if (rank != 0)
+	{
+		p = malloc(numpoints * sizeof(point) );
+		printf("rank %d numpoint = %d\n", rank, numpoints);
+	}
 
+    MPI_Bcast(p, numpoints, point_type, 0, comm);
+
+	if (rank == 2 )
+	{
+		printf("rank %d numpoint = %d\n", rank, numpoints);
+		printf(" %f -=- %f \n", p[0].x, p[0].y);
+		point *mx;
+		int size_mx = 0;
+
+		aki(&p, &numpoints, &mx, &size_mx);
+
+	}
 
 
  	double finish = MPI_Wtime();
 	/* --------------  */
 
  	if (!rank)
- 		printf ("Run time: %g" , finish-start); 
+ 		printf ("Run time: %g\n" , finish-start); 
 
 	/////////////////////////////////	
 	MPI_Type_free(&point_type);
 	MPI_Finalize();
 	return 0;
+}
+/******************************************************************************/
+void aki(point **a, int *size_a, point **ex, int *size_ex )
+{
+	int i=0;
+	point *temp= *a;
+	point xmax = temp[0],
+		  ymax = temp[0],
+		  xmin = temp[0],
+		  ymin = temp[0];
+
+	point	origen;
+	origen.x =0;
+	origen.y =0;
+
+	for (i=0; i<*size_a; i++)
+	{
+		if (temp[i].x > xmax.x ) xmax = temp[i];
+		if (temp[i].x < xmin.x ) xmin = temp[i];
+
+		if (temp[i].y > ymax.y ) ymax = temp[i];
+		if (temp[i].y < ymin.y ) ymin = temp[i];
+	}
+	int new_size_a	=0;	
+	for (i=0; i<*size_a; i++)
+	{
+		if ( 	!( (temp[i].x == xmax.x) && (temp[i].y == xmax.y) )
+			&& 	!( (temp[i].x == ymax.x) && (temp[i].y == ymax.y) )
+			&& 	!( (temp[i].x == xmin.x) && (temp[i].y == xmin.y) )
+			&& 	!( (temp[i].x == ymin.x) && (temp[i].y == ymin.y) ) )
+		{
+			if ( (isInside(xmax, ymax, temp[i], origen))
+				&&	(isInside(xmin, ymax, temp[i], origen))
+				&&	(isInside(xmin, ymin, temp[i], origen))
+				&&	(isInside(xmax, ymin, temp[i], origen)) )
+			{
+				printf ("Inside %d  = %f ; %f \n", i+1 , (*a)[i].x, (*a)[i].y);
+			}
+			else
+				new_size_a++;
+		}
+			
+	}
+
+// 	printf ("X max  = %f ; %f \n", xmax.x, xmax.y);
+// 	printf ("X min  = %f ; %f \n", xmin.x, xmin.y);
+// 
+// 	printf ("Y max  = %f ; %f \n", ymax.x, ymax.y);
+// 	printf ("Y min  = %f ; %f \n", ymin.x, ymin.y);
+
 }
 /******************************************************************************/
 void collectPoints(point **a, int *size,  unsigned rank)
@@ -98,5 +167,5 @@ bool isInside(point a, point b, point c, point origen)
 	bool abd = ((origen.y - a.y)*(b.x - a.x)) > ((b.y - a.y)*(origen.x - a.x));
 	bool abc = ((c.y - a.y)*(b.x - a.x)) > ((b.y - a.y)*(c.x - a.x));
 
-	return ( acd != bcd )&&( abc != abd);
+	return !( ( acd != bcd )&&( abc != abd) );
 }
